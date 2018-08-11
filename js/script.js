b616c63a25163346991988e281186e03f4c11d97
Renato Aguiar <renatooaa@outlook.com>
2018-08-11 10:57:35 -0300

    Commit Inicial

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const URL = "http://localhost:4500/bula"
+
+function find(question) {
+    var data = {"question" : question};
+
+    $.ajax({
+        type: "POST",
+        url: URL,
+        data: data,
+        success: success,
+        error: error,
+        dataType: "json"
+    });
+}
+
+function error(jqXHR, exception){
+    console.log("Status: " + jqXHR.status);
+    console.log("Message: " + jqXHR.responseText);
+    if(jqXHR.responseText != undefined){
+        $("#txtResposta").text(jqXHR.responseText)
+        $('#divResposta').removeClass("d-none");
+    }
+}
+
+function success(data) {
+    console.log(data);
+    $("#txtResposta").text(data.response)
+    $('#divResposta').removeClass("d-none");
+}
+
+$('#btnEnviar').on('click', function(){
+    var question = $('#txtPergunta').val();
+    if(question != '' && question != undefined){
+        find(question);
+    }
+});
+
+$('#btnLimpar').on('click', function(){	
+    $('#txtResposta' ).empty();
+    $('#divResposta').addClass("d-none");
+    $('#txtPergunta').val('');
+});
